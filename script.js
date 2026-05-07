0d6396870d8d69c99f99a0bf86f485097c9dd28b
Guilherme <guilhermecardoso0033@gmail.com>
2026-05-07 17:28:37 -0300

    function abrir carrinho

--- script.js
+++ script.js
@@ -31,4 +31,17 @@ function scrollRightCards(){
     });
 }
     
-    
+
+function abrirCarrinho() {
+  document.getElementById("cartSidebar").classList.add("active");
+  document.getElementById("cartOverlay").classList.add("active");
+}
+
+function fecharCarrinho() {
+    document.getElementById("cartSidebar").classList.remove("active");
+    document.getElementById("cartOverlay").classList.remove("active");
+}
+
+function finalizar() {
+    window.alert("seu carrinho está vazio !!! " )
+}
