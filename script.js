eb9d46971624d15a305f0f46648dd19f169d4734
Kauã Guilherme Almeida Melo <kauagmelotrabalho@gmail.com>
2026-05-07 17:23:54 -0300

    services bar

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 AOS.init();
+
+const cards = document.getElementById('cards');
+
+cards.addEventListener('scroll', () => {
+ const isAtEnd = cards.scrollLeft + cards.clientWidth >= cards.scrollWidth - 10
+ if (isAtEnd){
+    setTimeout(() => {
+        cards.scrollTo({
+            left: 0,
+            behavior: "smooth"
+        });
+    },200);
+ }
+
+
+})
+
+function scrollLeftCards(){
+    cards.scrollBy({
+        left: -320,
+        behavior: "smooth"
+    });
+}
+
+function scrollRightCards(){
+
+    cards.scrollBy({
+        left: 320,
+        behavior: "smooth"
+    });
+}
+    
+    
