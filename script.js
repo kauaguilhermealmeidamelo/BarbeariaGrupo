02faa648a775961dce36b1e69f43e0294ca0be4f
Guilherme Cardoso Ramos <guilherme55361176@edu.df.senac.br>
2026-05-07 16:57:03 -0300

    botão funcional

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 AOS.init();
+
+function abrirCarrinho() {
+  document.getElementById("cartSidebar").classList.add("active");
+  document.getElementById("cartOverlay").classList.add("active");
+}
+
+function fecharCarrinho() {
+    document.getElementById("cartSidebar").classList.remove("active");
+    document.getElementById("cartOverlay").classList.remove("active");
+}
+
+function finalizar() {
+    window.alert("seu carrinho está vazio !!! " )
+}
